f00a65a142898b933201e071a5def5626f2e0a29
hydra <16paysayocesar@gmail.com>
2026-08-29 19:03:07 +0100

    feat :

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
+import Footer from "@/components/layout/Footer";
+import APropos from "@/components/sections/APropos";
+import Activites from "@/components/sections/Activites";
+import AgricultureFeature from "@/components/sections/AgricultureFeature";
+import Contact from "@/components/sections/Contact";
 import Hero from "@/components/sections/Hero";
+import PourquoiNousChoisir from "@/components/sections/whyus";
+import FurrowDivider from "@/components/ui/FurrowDivider";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+
+      <FurrowDivider />
+
+      <AgricultureFeature />
+
+      <FurrowDivider flipped />
+
+      <Activites />
+
+      <APropos />
+
+      <PourquoiNousChoisir />
+
+      <Contact />
+
+      <Footer />
     </>
   );
 }
