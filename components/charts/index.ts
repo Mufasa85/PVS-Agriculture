14b247907f815af6cc48b3488ac6a494c10f33ba
hydra <16paysayocesar@gmail.com>
2026-09-08 16:39:08 +0100

    feat : bklit

---
+++ components/charts/index.ts
@@ -0,0 +1,12 @@
+// components/charts/index.ts
+export { Background } from "./background";
+export { ChartBrushLayout } from "./chart-brush-layout";
+export { Line } from "./line";
+export { LineChart } from "./line-chart";
+export { XAxis } from "./x-axis";
+// Ajoutez ici tous les autres composants que vous utilisez
+
+export { ChartBrush } from "./chart-brush";
+
+export { ChartLegend } from "./chart-legend";
+export { ChartTooltip } from "./tooltip";
